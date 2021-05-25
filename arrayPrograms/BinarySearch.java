eb4cf89068a7f175264d6ab3c2b86a5f64b4147c
itssamyakjain <80161658+itssamyakjain@users.noreply.github.com>
2021-05-25 19:24:54 +0530

    added iterative approach

--- arrayPrograms/BinarySearch.java
+++ arrayPrograms/BinarySearch.java
@@ -1,25 +1,21 @@
 import java.util.*;
 
 public class BinarySearch{
-	// method for binary search
-  // returns the index of element if it is found else returns -1
-	static int binarySearch(int[] a,int x,int start, int end) {
-	  if(start<=end) {
-		  int mid = (start+end)/2;
-		  if(a[mid]==x) {
-			  return mid;
-		  }
-		  else if(x<a[mid]) {
-			  end=mid-1;
-			  return binarySearch(a,x,start,end);
-		  }
-		  else{
-			  start=mid+1;
-			  return binarySearch(a,x,start,end);
-		  }
-	   }
-	   return -1;
-   }
+	// method for binary search iterative approach
+  	// returns the index of element if it is found else returns -1
+	static int binarySearch(int[] arr,int x, int start,int end) {
+		
+		while(start<=end) {
+			int mid = (start+end)/2;
+			if(arr[mid]==x)
+				return mid;
+			else if(arr[mid]>x)
+				end = mid-1;
+			else
+				start = mid+1;
+		}
+		return -1;
+	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -30,7 +26,7 @@ public class BinarySearch{
 			arr[i]=sc.nextInt();
 	  // input element to be searched
 		int x= sc.nextInt();
-    // sort the array
+   	 // sort the array
 		Arrays.sort(arr);
 		System.out.println(binarySearch(arr,x,0,n-1));
 	
