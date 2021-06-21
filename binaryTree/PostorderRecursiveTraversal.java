c73b2c0555449694523ad3ae2e8fbe27972b62e6
itssamyakjain <80161658+itssamyakjain@users.noreply.github.com>
2021-06-21 16:40:34 +0530

    postorder

---
+++ binaryTree/PostorderRecursiveTraversal.java
@@ -0,0 +1,36 @@
+// construction of a tree
+class Node{
+	int key;
+	Node left;
+	Node right;
+	
+	public Node(int key) {
+		this.key=key;
+	}
+	
+}
+
+
+public class PostorderRecursiveTraversal {
+
+  // POSTORDER TRAVERSAL
+	public static void inorder(Node root) {
+		if(root!=null) {
+      inorder(root.left);
+      inorder(root.right);
+      System.out.print(root.key+" ");
+    }
+	}	
+  
+  	public static void main(String[] args) {
+	  	Node root = new Node(10);
+		  root.left = new Node(15);
+		  root.right = new Node(20);
+	  	root.left.left = new Node(25);
+	  	root.left.right = new Node(30);
+	  	root.right.left = new Node(27);
+		  root.right.right = new Node(31);
+		  inorder(root);      // OUTPUT POSTORDER TRAVERSAL (LEFT RIGHT ROOT) :  25 30 15 27 31 20 10 
+	}
+
+}
