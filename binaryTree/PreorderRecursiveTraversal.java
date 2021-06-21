b179a5366d6c13e5f29031d051bc29fd378d5920
itssamyakjain <80161658+itssamyakjain@users.noreply.github.com>
2021-06-21 16:38:32 +0530

    preorder

---
+++ binaryTree/PreorderRecursiveTraversal.java
@@ -0,0 +1,37 @@
+// construction of a tree
+class Node{
+	int key;
+	Node left;
+	Node right;
+	
+	public Node(int key) {
+		this.key=key;
+	}
+	
+}
+
+
+public class PreorderRecursiveTraversal {
+  
+  // PREORDER TRAVERSAL
+	public static void inorder(Node root) {
+		if(root!=null) {
+      System.out.print(root.key+" ");
+      inorder(root.left);
+      inorder(root.right);
+    }
+	}	
+  
+  	public static void main(String[] args) {
+	  	Node root = new Node(10);
+		  root.left = new Node(15);
+		  root.right = new Node(20);
+	  	root.left.left = new Node(25);
+	  	root.left.right = new Node(30);
+	  	root.right.left = new Node(27);
+		  root.right.right = new Node(31);
+		  inorder(root);      // OUTPUT PREORDER TRAVERSAL (ROOT LEFT RIGHT) :  10 15 25 30 20 27 31
+	}
+
+}
+
